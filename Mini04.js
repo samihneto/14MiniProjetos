7ffe2ae54bdace1b293b8409b07cbb0b34112875
samihneto <samihneto@gmail.com>
2024-01-22 11:33:27 -0300

    14MP.0.2

--- Mini04.js
+++ Mini04.js
@@ -4,3 +4,16 @@ a cada 5km acima do limite você ganha 1 ponto
 Math.floor()
 Caso pontos maior que 12 -> "Carteira Suspensa"
 */
+
+verificarVelocidade(60);
+
+function verificarVelocidade(velocidade) {
+    if (velocidade <= 70) {
+        console.log('Ok');
+    } else if (velocidade ) {
+        const pontos = ((velocidade - 70) / 5);
+        if (pontos >= 12) {
+            console.log('Carteira Suspensa');
+        }
+    }
+}
--- Mini05.js
+++ Mini05.js
@@ -2,3 +2,27 @@
 Receber uma quantidade de valores para avaliar
 Função exibe se cada valor é par ou ímpar.
 */
+
+console.log(avaliarValor(0));
+
+function avaliarValor (numero) {
+    if (numero===0) {
+        return 'Você escolheu 0!'
+    } else if (numero%2===0) {
+        return 'Esse número é par!'
+    } else {
+        return 'Esse número é ímpar!'
+    }
+}
+
+exibirTipo(5);
+function exibirTipo(limite) {
+    for (let i = 0; i <= limite; i++) {
+        if(i%2===0) {
+            console.log(i, 'Par!');
+        } else {
+            console.log(i, 'Ímpar!')
+        }
+        
+    }
+}
